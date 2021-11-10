Fix gradient zeroing for SGD/Adam

intervalnet/models/interval.py
@@ -67,21 +67,23 @@ class IntervalLinear(nn.Module):
     def switch_mode(self, mode: Mode) -> None:
         self.mode = mode
 
+        def enable(params: list[Parameter]):
+            for p in params:
+                p.requires_grad_()
+
+        def disable(params: list[Parameter]):
+            for p in params:
+                p.requires_grad_(False)
+                p.grad = None
+
+        disable([self.weight, self._radius, self._shift, self._scale])
+
         if mode == Mode.VANILLA:
-            self.weight.requires_grad = True
-            self._radius.requires_grad = False
-            self._shift.requires_grad = False
-            self._scale.requires_grad = False
+            enable([self.weight])
         elif mode == Mode.EXPANSION:
-            self.weight.requires_grad = False
-            self._radius.requires_grad = True
-            self._shift.requires_grad = False
-            self._scale.requires_grad = False
+            enable([self._radius])
         elif mode == Mode.CONTRACTION:
-            self.weight.requires_grad = False
-            self._radius.requires_grad = False
-            self._shift.requires_grad = True
-            self._scale.requires_grad = True
+            enable([self._shift, self._scale])
 
     def freeze_task(self) -> None:
         with torch.no_grad():

train.py
@@ -19,7 +19,7 @@ from rich import print
 from torch import Tensor
 from torch.optim import SGD, Adam
 
-from intervalnet.cfg import DatasetType, ModelType, Settings
+from intervalnet.cfg import DatasetType, ModelType, OptimizerType, Settings
 from intervalnet.datasets import mnist
 from intervalnet.metrics.basic import EvalAccuracy, TotalLoss, TrainAccuracy
 from intervalnet.metrics.interval import interval_training_diagnostics
@@ -63,7 +63,11 @@ class Experiment(AvalancheExperiment):
                 hidden_dim=400,
                 output_classes=self.n_output_classes,
             )
-            optimizer = SGD(self.model.parameters(), lr=self.cfg.learning_rate)
+            if self.cfg.optimizer is OptimizerType.SGD:
+                optimizer = SGD(self.model.parameters(), lr=self.cfg.learning_rate, momentum=self.cfg.momentum)
+            else:
+                assert self.cfg.optimizer == OptimizerType.ADAM
+                optimizer = Adam(self.model.parameters(), lr=self.cfg.learning_rate)
             strategy_ = functools.partial(
                 Naive,
                 criterion=nn.CrossEntropyLoss(),
@@ -75,8 +79,11 @@ class Experiment(AvalancheExperiment):
                 hidden_dim=400,
                 output_classes=self.n_output_classes,
             )
-            # optimizer = SGD(self.model.parameters(), lr=self.cfg.learning_rate)
-            optimizer = Adam(self.model.parameters(), lr=self.cfg.learning_rate)
+            if self.cfg.optimizer is OptimizerType.SGD:
+                optimizer = SGD(self.model.parameters(), lr=self.cfg.learning_rate, momentum=self.cfg.momentum)
+            else:
+                assert self.cfg.optimizer == OptimizerType.ADAM
+                optimizer = Adam(self.model.parameters(), lr=self.cfg.learning_rate)
             strategy_ = functools.partial(
                 IntervalTraining,
                 enable_visdom=self.cfg.enable_visdom,