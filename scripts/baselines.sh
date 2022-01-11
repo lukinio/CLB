6f30349244b8fdc5f0bd9ae71a4cb6130c627614
Karol J. Piczak <karol@piczak.com>
2022-01-12 00:21:55 +0100

    Fix baseline group naming

--- scripts/baselines.sh
+++ scripts/baselines.sh
@@ -5,8 +5,8 @@ trap "kill 0" INT
 for scenario in INC_TASK INC_DOMAIN INC_CLASS; do
     for cfg in sgd adam ewc; do
         for seed in 2001 2002 2003 2004 2005; do
-            python train.py cfg=baseline_$cfg group=$scenario_$cfg cfg.seed=$seed cfg.scenario=$scenario tags=["$scenario"] > /dev/null 2>&1 &
-            echo "Running $scenario_$cfg with seed = $seed"
+            python train.py cfg=baseline_${cfg} group=${scenario}_${cfg} cfg.seed=${seed} cfg.scenario=${scenario} tags=["${scenario}"] > /dev/null 2>&1 &
+            echo "Running ${scenario}_${cfg} with seed = $seed"
         done
         wait
     done
