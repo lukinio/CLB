c2770b7570d5976094f0de6aa520102957d73010
Maciej Wolczyk <maciej.wolczyk@gmail.com>
2022-01-20 14:15:24 +0100

    Change lr & add interval training script for c100

--- scripts/interval_cifar100.sh
+++ scripts/interval_cifar100.sh
@@ -9,3 +9,14 @@
 trap "kill 0" INT
 
 # TODO
+offline=False
+scenario=INC_TASK
+
+idx=0
+for elr in 0.01 0.1 1. 5.; do
+	export CUDA_VISIBLE_DEVICES=${idx}
+	python train.py cfg=default_cifar100 cfg.scenario=${scenario} cfg.offline=${offline} \
+		cfg.strategy=Interval cfg.interval.expansion_learning_rate=${elr} cfg.interval.robust_lambda=1. \
+		tags=["20220120_interval_cifar100_it"] &
+	idx=$((idx+1))
+done
