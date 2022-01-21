a7cb5d32ccb25926157fecc42bab3f558cda65c6
Karol J. Piczak <karol@piczak.com>
2022-01-21 15:21:46 +0100

    Fix SynapticIntelligence (cherry pick Avalanche #735)

--- intervalnet/strategies/__init__.py
+++ intervalnet/strategies/__init__.py
@@ -1,4 +1,6 @@
 from .ewc import EWCPlugin as EWCPlugin
+from .ewc import L2Plugin as L2Plugin
 from .joint import JointTraining as JointTraining
 from .lwf import LwFPlugin as LwFPlugin
+from .si import SynapticIntelligencePlugin as SynapticIntelligencePlugin
 from .vanilla import VanillaTraining as VanillaTraining
--- train.py
+++ train.py
@@ -10,7 +10,6 @@ from avalanche.benchmarks.scenarios.new_classes.nc_scenario import NCExperience
 from avalanche.benchmarks.utils.avalanche_dataset import AvalancheDataset
 from avalanche.evaluation.metric_definitions import PluginMetric
 from avalanche.training.plugins.evaluation import EvaluationPlugin
-from avalanche.training.plugins.synaptic_intelligence import SynapticIntelligencePlugin
 from pytorch_yard import info, info_bold
 from pytorch_yard.avalanche import RichLogger, incremental_domain
 from pytorch_yard.avalanche.scenarios import incremental_class, incremental_task
@@ -31,8 +30,14 @@ from intervalnet.metrics.basic import EvalAccuracy, TotalLoss, TrainAccuracy
 from intervalnet.metrics.interval import interval_training_diagnostics
 from intervalnet.models.interval import IntervalMLP
 from intervalnet.models.mlp import MLP
-from intervalnet.strategies import EWCPlugin, JointTraining, LwFPlugin, VanillaTraining
-from intervalnet.strategies.ewc import L2Plugin
+from intervalnet.strategies import (
+    EWCPlugin,
+    JointTraining,
+    L2Plugin,
+    LwFPlugin,
+    SynapticIntelligencePlugin,
+    VanillaTraining,
+)
 from intervalnet.strategy import IntervalTraining
 
 assert pytorch_yard.__version__ == "2021.12.31.1", "Code not tested with different pytorch-yard versions."  # type: ignore # noqa
